b918d7d6bca597cbf5cf4122e7bd0798fc0f8427
thisiseunji <monahk93@gmail.com>
2023-06-18 16:53:32 +0900

    feat : 리뷰 insert 구현
    1. 이미지 저장 됨
    2. 예약당 1개의 리뷰만 저장되도록 해야함(프론트)

--- src/main/java/com/kej/myong/review/controller/ReviewController.java
+++ src/main/java/com/kej/myong/review/controller/ReviewController.java
@@ -1,11 +1,17 @@
 package com.kej.myong.review.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kej.myong.review.model.service.ReviewService;
@@ -23,26 +29,29 @@ public class ReviewController {
 	private FileSaver fileSaver;
 	
 	
-	@GetMapping()
+	@GetMapping
 	public String reviewEnrollForm(){
 		return "review/reviewEnrollForm";
 		
 	}
 
 	@PostMapping(produces = "text/html; charset=utf-8", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-	public String insertReview(Review r){
-		
-		System.out.println(r);
+	@ResponseBody
+	public String insertReview(Review r, HttpSession session){
 		
 		MultipartFile multipartFile = r.getMultipartFile();
-		 
-		System.out.println(multipartFile);
 		
-		// 파일 저장하기 : 파일을 저장 경로를 반환
-		r.setReviewImg(fileSaver.storeFile(multipartFile, "review"));
-		
+		// 이미지 첨부를 했을 경우
+		if (multipartFile != null) {
+			String filepath = fileSaver.storeFile(multipartFile, "review", session);
+			if (filepath == null) {
+				return "리뷰 등록 실패";
+			} else {
+				r.setReviewImg(filepath);
+			}
+		}
+			
 		int result = reviewService.insertReview(r);
-		
 		return result > 0? "리뷰 등록 성공" : "리뷰 등록 실패";
 	}	
 }
--- src/main/java/com/kej/myong/utils/FileSaver.java
+++ src/main/java/com/kej/myong/utils/FileSaver.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,9 +15,8 @@ public class FileSaver {
 	
 	// 저장경로
 	// private final String FILE_DIR_PATH = "/resources/img/";
-	private final String FILE_DIR_PATH = "src/main/webapp/resources/img/";
-	private final String REVIEW = "review_upfiles/";
-	private final String STYLE = "style_upfiles/"; 
+	private final String REVIEW = "/resources/img/review_upfiles/";
+	private final String STYLE = "/resources/img/style_upfiles/"; 
 	
 	// 저장 할 파일 이름 구성
 	private String createStoreFilename(String originalFilename) {
@@ -25,14 +26,14 @@ public class FileSaver {
 	}
 	
 	// 파일 저장 경로 구성 
-	private String createFilePath(String storeFilename, String fileFor) {
+	private String createFilePath(String fileFor, HttpSession session) {
 		String viaPath = fileFor.equals("review") ? REVIEW : STYLE;
 		
-		return FILE_DIR_PATH + viaPath + storeFilename;
+		return session.getServletContext().getRealPath(viaPath);
 	}
 	
 	// 실제 파일 저장
-	public String storeFile(MultipartFile multipartFile, String fileFor) {
+	public String storeFile(MultipartFile multipartFile, String fileFor, HttpSession session){
 		
 	    if (multipartFile.isEmpty()) {
 	        return null;
@@ -40,12 +41,13 @@ public class FileSaver {
 	    
 	    String originalFilename = multipartFile.getOriginalFilename();
 	    String storeFilename = createStoreFilename(originalFilename);
-	    String filePath = createFilePath(storeFilename, fileFor);
+	    String filePath = createFilePath(fileFor, session);
+
 	    try {
-			multipartFile.transferTo(new File(filePath));
+			multipartFile.transferTo(new File(filePath, storeFilename));
 		} catch (IOException e) {
 			e.printStackTrace();
-			return fileFor +" 파일생성 오류";
+			return null;
 		}
 	    
 		return filePath;
